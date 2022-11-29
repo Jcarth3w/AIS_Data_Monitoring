845acfe1543d2bc5ad7c37a39f8d9ad68fb9e44d
Jcarth3w <jdcarthew@bsu.edu>
2022-11-29 12:08:14 -0500

    Added template

---
+++ DAO.py
@@ -0,0 +1,24 @@
+import json
+import mysql.connector
+from mysql.connector import errorcode
+import unittest 
+
+class MessageDAO (test_mode)
+
+	
+	
+	
+	
+	
+	
+	
+##################TESTS#####################
+
+
+def test_insert_msg (self):
+	
+	
+
+
+
+
